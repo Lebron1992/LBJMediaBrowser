02fce52c4f636dfc547874a263a5cad2716ff5bd
Lebron <wenzhi.zeng@outlook.com>
2021-10-12 11:45:42 +0800

    refactor code

--- Sources/LBJMediaBrowser/Library/PHImageManager+PHImageManagerType.swift
+++ Sources/LBJMediaBrowser/Library/PHImageManager+PHImageManagerType.swift
@@ -1,24 +1,6 @@
 import Photos
 import UIKit
 
-protocol PHImageManagerType {
-  func requestImage(
-    for asset: PHAsset,
-    targetSize: CGSize,
-    contentMode: PHImageContentMode,
-    options: PHImageRequestOptions?,
-    completion: @escaping (Result<UIImage, Error>) -> Void
-  ) -> PHImageRequestID
-
-  func requestAVAsset(
-    forVideo asset: PHAsset,
-    options: PHVideoRequestOptions?,
-    completion: @escaping (Result<URL, Error>) -> Void
-  ) -> PHImageRequestID
-
-  func cancelImageRequest(_ requestID: PHImageRequestID)
-}
-
 extension PHImageManager: PHImageManagerType {
 
   func requestImage(
---
+++ Sources/LBJMediaBrowser/Library/PHImageManagerType.swift
@@ -0,0 +1,20 @@
+import Photos
+import UIKit
+
+protocol PHImageManagerType {
+  func requestImage(
+    for asset: PHAsset,
+    targetSize: CGSize,
+    contentMode: PHImageContentMode,
+    options: PHImageRequestOptions?,
+    completion: @escaping (Result<UIImage, Error>) -> Void
+  ) -> PHImageRequestID
+
+  func requestAVAsset(
+    forVideo asset: PHAsset,
+    options: PHVideoRequestOptions?,
+    completion: @escaping (Result<URL, Error>) -> Void
+  ) -> PHImageRequestID
+
+  func cancelImageRequest(_ requestID: PHImageRequestID)
+}
